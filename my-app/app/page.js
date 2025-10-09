aa8dba5116a96fe98218e628e9e12a51f5655421
MichaelC609 <mjcastillo@cpp.edu>
2025-10-09 14:02:39 -0700

    updated home page

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import RouteOptimizer from "./frontend/route-optimizer/page";
 
 export default function Home() {
   return (
     <>
-    
+      <RouteOptimizer />
     </>
   );
 }
+
