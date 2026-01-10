9ff23e532ec3e081ace57088c59dc6180aa94f23
MichaelC609 <mjcastillo@cpp.edu>
2026-01-09 16:51:43 -0800

    fixed user model

--- my-app/backend/backend/routes/models.py
+++ my-app/backend/backend/routes/models.py
@@ -1,19 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-#Class for Routes/Route Preferences
-class Route(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='routes')
-    name = models.CharField(max_length=255)
-    start_location = models.CharField(max_length=255)
-    end_location = models.CharField(max_length=255)
-    distance = models.FloatField()
-    duration = models.FloatField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return f"Route {self.route_id}: {self.start_location} → {self.end_location}"
-
 class User(AbstractUser):
     #User model to store additional Google info
     email = models.EmailField(unique=True) #make email unique and required
@@ -32,3 +19,16 @@ class User(AbstractUser):
         db_table = 'users'
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+
+#Class for Routes/Route Preferences
+class Route(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='routes')
+    name = models.CharField(max_length=255)
+    start_location = models.CharField(max_length=255)
+    end_location = models.CharField(max_length=255)
+    distance = models.FloatField()
+    duration = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Route {self.route_id}: {self.start_location} → {self.end_location}"
